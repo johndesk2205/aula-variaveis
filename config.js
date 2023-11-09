d857d133623b04f153c4d30c5db73006a298ed17
igor <igoralves.martins@hotmail.com>
2023-11-08 23:20:29 -0300

    aula03

---
+++ config.js
@@ -0,0 +1,7 @@
+let nome 
+let idade 
+let cidade 
+nome = prompt (`Qual seu nome?`)
+idade = prompt (`qual sua idade?`)
+cidade = prompt (`Onde você mora?`)
+console.log(`Ola meu nome é ${nome} , tenho ${idade} anos e moro em ${cidade}.`);   
